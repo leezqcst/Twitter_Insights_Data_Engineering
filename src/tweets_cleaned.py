a662766cdd0d4311f6adc40ddfbddd8d08307556
ricgu8086 <ricgu8086@gmail.com>
2015-11-01 18:57:15 +0100

    Refactor the structure of the code. The main code is now a function

--- src/tweets_cleaned.py
+++ src/tweets_cleaned.py
@@ -16,8 +16,6 @@ import sys
 BATCH_SIZE = 1
 
 
-path_2_input = sys.argv[1]
-path_2_output = sys.argv[2]
 unicode_tweets = 0
 
 #Functions
@@ -43,78 +41,83 @@ def removeEscape(cad):
     pattern = "[^a-zA-Z0-9 #@:,.*/'!?]"
     return re.sub(pattern, '', cad)
 
+def clean():
+
+    global BATCH_SIZE
+
+    path_2_input = sys.argv[1]
+    path_2_output = sys.argv[2]
+
+    #Checking the paths given by the user
+    if not os.path.isfile(path_2_input):
+        print("The given input file does not exist\n")
+        sys.exit()
+
+    try:
+        f = open(path_2_output, 'w')
+        f.close()
+    except IOError:
+        print("The given output file does not exist or can't be created\n")
+        sys.exit()
+
+    cleaned_tweets = []
+    unprocessed_tweets = []
+    end_reached = False
 
 
-## Functions
+    with open(path_2_input, 'r') as f_input, open(path_2_output, 'w') as f_output:
+
+        #Processing input
+        #################
+        while not end_reached:
+
+            del unprocessed_tweets[:]
+            del cleaned_tweets[:]
+
+            #Read a batch
+
+            ind = 0
+
+            while (not end_reached) and (ind < BATCH_SIZE):
+
+                aux = f_input.readline()
+
+                if not aux:
+                    end_reached = True
+                else:
+                    unprocessed_tweets.append(aux)
+                    ind += 1
+
+            if len(unprocessed_tweets):
+                #Clean a batch
+
+                df = pd.DataFrame(json.loads(line) for line in unprocessed_tweets)
+
+                #Remove unwanted columns
+                df = df[['text', 'created_at']]
+
+                #Remove unicode characters
+                df['text'] = df['text'].apply(removeUnicode)
+
+                #Remove escape characters
+                df['text'] = df['text'].apply(removeEscape)
+
+                cleaned_tweets_ds = df['text'] + ' (timestamp: ' + df['created_at'] + ')'
+                cleaned_tweets = [x for x in cleaned_tweets_ds]
 
 
+                #Processing output
+                ##################
 
-#Checking the paths given by the user
-if not os.path.isfile(path_2_input):
-    print("The given input file does not exist\n")
-    sys.exit()
+                #Write cleaned batch
+                for tweet in cleaned_tweets:
+                    f_output.write(tweet + '\n')
 
-try:
-    f = open(path_2_output, 'w')
-    f.close()
-except IOError:
-    print("The given output file does not exist or can't be created\n")
-    sys.exit()
+        #Add summary
+        f_output.write("\n%d tweets contained unicode." % unicode_tweets)
 
-cleaned_tweets = []
-unprocessed_tweets = []
-end_reached = False
+    print('Full batch processed')
 
 
-with open(path_2_input, 'r') as f_input, open(path_2_output, 'w') as f_output:
-
-    #Processing input
-    #################
-    while not end_reached:
-
-        del unprocessed_tweets[:]
-        del cleaned_tweets[:]
-
-        #Read a batch
-
-        ind = 0
-
-        while (not end_reached) and (ind < BATCH_SIZE):
-
-            aux = f_input.readline()
-
-            if not aux:
-                end_reached = True
-            else:
-                unprocessed_tweets.append(aux)
-                ind += 1
-
-        if len(unprocessed_tweets):
-            #Clean a batch
-
-            df = pd.DataFrame(json.loads(line) for line in unprocessed_tweets)
-
-            #Remove unwanted columns
-            df = df[['text', 'created_at']]
-
-            #Remove unicode characters
-            df['text'] = df['text'].apply(removeUnicode)
-
-            #Remove escape characters
-            df['text'] = df['text'].apply(removeEscape)
-
-            cleaned_tweets_ds = df['text'] + ' (timestamp: ' + df['created_at'] + ')'
-            cleaned_tweets = [x for x in cleaned_tweets_ds]
-
-
-            #Processing output
-            ##################
-
-            #Write cleaned batch
-            for tweet in cleaned_tweets:
-                f_output.write(tweet + '\n')
-
-    #Add summary
-    f_output.write("\n%d tweets contained unicode." % unicode_tweets)
-
-print('Full batch processed')
+if __name__ == '__main__':
+    clean()
